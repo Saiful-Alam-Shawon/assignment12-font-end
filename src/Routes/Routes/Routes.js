66eb33740fed88340b20a71f1876ae9f1194cb86
Saiful-Alam-Shawon <fullstackebdevelopment1@gmail.com>
2022-12-02 08:48:13 +0600

    last assignment commit

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -74,7 +74,7 @@ const router = createBrowserRouter([
             // },
             // {
             //     path: 
-            //     element: <Seller></Seller>,
+            //     element: ,
             // },
             // {
             //     path: 
